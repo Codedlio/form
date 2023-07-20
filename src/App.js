4a0895a74e802cfe5ef233f13caf97ee6628b737
Codedlio <alexis.leonel_@hotmail.com>
2023-07-19 18:01:27 -0600

    Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+const App = () => {
+  const [nombre, setNombre] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [opciones, setOpciones] = useState('');
+  const [mostrarInfo, setMostrarInfo] = useState(false);
+  const [listaNombres, setListaNombres] = useState([]);
+  const handleNombreChange = (event) => {
+    setNombre(event.target.value);
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const handleTelefonoChange = (event) => {
+    setTelefono(event.target.value);
+  };
+
+  const handleOpcionesChange = (event) => {
+    setOpciones(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    setMostrarInfo(true); 
+  
+   setListaNombres([...listaNombres, { nombre, telefono, opciones }]);
+
+    
+    setNombre('');
+    setTelefono('');
+    setOpciones('');
+  };
+   const handleSortAZ = () => {
+    
+    const sortedList = listaNombres.slice().sort((a, b) => a.nombre.localeCompare(b.nombre));
+    setListaNombres(sortedList);
+  };
+
+  const handleDeleteName = (index) => {
+    
+    const updatedList = listaNombres.filter((_, i) => i !== index);
+    setListaNombres(updatedList);
+  };
+
+ return (
+    <div>
+      <h1>Formulario React</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nombre:</label>
+          <input type="text" value={nombre} onChange={handleNombreChange} />
+        </div>
+        <div>
+          <label>Teléfono:</label>
+          <input type="text" value={telefono} onChange={handleTelefonoChange} />
+        </div>
+        <div>
+          <label>Opciones:</label>
+          <select value={opciones} onChange={handleOpcionesChange}>
+            <option value="opcion1">Empresa</option>
+            <option value="opcion2">Persona</option>
+          </select>
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+
+      {mostrarInfo && (
+        <div>
+          <h2>Información del formulario:</h2>
+          <ul>
+            {listaNombres.map((item, index) => (
+              <li key={index}>
+                {item.nombre} - {item.telefono} - {item.opciones}{' '}
+                <button onClick={() => handleDeleteName(index)}>Borrar</button>
+              </li>
+            ))}
+          </ul>
+          <button onClick={handleSortAZ}>Ordenar A-Z</button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
